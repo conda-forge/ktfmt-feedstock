7ddf4878d755f5a3d6b9b8d52ade624a979cf787
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-10 10:02:41 -0800

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,23 +2,25 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-mkdir -p ${PREFIX}/libexec/${PKG_NAME}
+mkdir -p ${PREFIX}/libexec/ktfmt
 mkdir -p ${PREFIX}/bin
 
-# Build with maven
-mvn -B install -DskipTests
-install -m 644 core/target/ktfmt-${PKG_VERSION}-jar-with-dependencies.jar ${PREFIX}/libexec/${PKG_NAME}/ktfmt.jar
+# Add dependency-license-report plugin
+sed -i 's/alias(libs.plugins.shadowJar) apply false/alias(libs.plugins.shadowJar) apply false\nid("com.github.jk1.dependency-license-report") version "latest.release"/' build.gradle.kts
 
-mvn license:download-licenses -Dgoal=download-licenses
+# Build with gradle
+./gradlew :ktfmt:shadowJar
+./gradlew generateLicenseReport
+
+install -m 644 core/build/libs/ktfmt-${PKG_VERSION}-with-dependencies.jar ${PREFIX}/libexec/ktfmt/ktfmt.jar
 
 # Create bash and batch files
-tee ${PREFIX}/bin/${PKG_NAME} << EOF
+tee ${PREFIX}/bin/ktfmt << EOF
 #!/bin/sh
 exec \${JAVA_HOME}/bin/java -jar \${CONDA_PREFIX}/libexec/ktfmt/ktfmt.jar "\$@"
 EOF
-chmod +x ${PREFIX}/bin/${PKG_NAME}
+chmod +x ${PREFIX}/bin/ktfmt
 
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
+tee ${PREFIX}/bin/ktfmt.cmd << EOF
 call %JAVA_HOME%\bin\java -jar %CONDA_PREFIX%\libexec\ktfmt\ktfmt.jar %*
 EOF
-
